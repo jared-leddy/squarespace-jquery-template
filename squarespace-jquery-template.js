9c492cc7b1d3b4c2a565cbe0a8376741bdcd99ef
Jared Ledbetter <57743183+jared-leddy@users.noreply.github.com>
2022-03-01 15:33:35 -0500

    Add files via upload

---
+++ squarespace-jquery-template.js
@@ -0,0 +1,21 @@
+// Footer
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script>
+	$(document).ready(function() {
+      if(typeof(pageCode)=='function'){
+       	pageCode();
+      }
+    });
+</script>
+
+// Forms Pages
+
+<script>
+function pageCode() {
+
+  } // end page code function
+</script>
